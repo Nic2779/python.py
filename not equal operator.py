e0384a4b4a888db9d3a4cc6b832065ff61610dc3
Nic2779 <nicdimitriou@icloud.com>
2025-01-25 09:41:36 +0200

    not equal operator.py

--- not equal operator.py
+++ not equal operator.py
@@ -3,3 +3,13 @@ b = 12
 c = 12
 print(a != b)
 print(b != c)
+
+a = "pyhton"
+b = "coding"
+if a != b:
+  print(a, 'and' , b, 'are diffrent')
+
+a = 4
+b = 5
+if (a == 1) != (b == 5):
+  print('hello')
