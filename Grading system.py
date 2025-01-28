760c34d059c96385de4b0994f14de5b523cc3a2d
Nic2779 <nicdimitriou@icloud.com>
2025-01-28 16:43:55 +0200

    Garding system

---
+++ Grading system.py
@@ -0,0 +1,13 @@
+print("Enter Marks Obtained in Maths and English")
+MarkOne = int(input())
+MarkTwo = int(input())
+
+tog = MarkOne+MarkTwo
+avg = tot/2
+
+if avg>=91 and avg<=100:
+ print("your grade is A1")
+elif avg>=81 and avg<=91:
+ print("your grade is A2")
+else:
+ print("your grade is B1")
---
+++ Identity operator.py
@@ -0,0 +1,17 @@
+x = 5
+if (type(x)is int):
+ print("true")
+else:
+ print("false")
+
+ x = 5.0
+if (type(x) is not float):
+ print("true")
+else:
+ print("flase")
+
+a = 10
+b = -10
+print("a >> 1 =" ,  a >> 1)
+print("b >> 1 =" ,  b >> 1)
+
